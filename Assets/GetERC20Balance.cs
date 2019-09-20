b0bb4f5b613811c467a67c36ccf795e86cd0b2f9
think-in-universe <mstr.raphael@gmail.com>
2019-09-20 11:35:43 +0800

    rewrite and clean up code

---
+++ Assets/GetERC20Balance.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+
+public class GetERC20Balance : MonoBehaviour
+{
+    public Text tokenBalanceText;
+
+    // Start is called before the first frame update
+    async Task Start()
+    {
+        var balance = await BalanceOfERC20();
+        Debug.Log($"I have {balance} Wei TEST Tokens");
+        tokenBalanceText = GetComponent<Text>();
+        tokenBalanceText.text = balance.ToString() + " Wei";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static async Task<System.Numerics.BigInteger> BalanceOfERC20()
+    {
+        var matic = Settings.GetMatic();
+        return await matic.BalanceOfERC20(Settings.FROM_ADDRESS, Settings.MATIC_TEST_TOKEN);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # matic-unity-starter-kit
 
-A Game Tutorial built with Unity3D and Matic.Net and Matic.Net
+A Game Tutorial built with Unity3D and Matic.Net, to democratize modern blockchain game with Matic. 
+
+
+## Level 1: A Basic Matic Demo with Matic.Net and Unity3D
+
+A Unity3D demo with Unity UI that demostrates how to get balance, deposit to child chain, transfer in Matic child chain, withdraw to parent chain, etc. 
+
+![image](https://user-images.githubusercontent.com/46699230/65247001-2ce3ac00-db22-11e9-8861-922b650153f4.png)
+
+
